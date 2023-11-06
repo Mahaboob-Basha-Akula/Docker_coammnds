3034979609d4ea475d705530c40e01bda3d17c9c
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2023-11-06 12:31:56 +0530

    docker commands

---
+++ docker_commands.js
@@ -0,0 +1,23 @@
+
+
+To install docker  ----------->>> sudo apt install docker.io -yield
+
+To know the version ---------->>> docker --version
+
+To check the docker info =------>>>> docker info
+
+To see all the images in local ----->>> docker images
+
+To find the images in docker hub ----->> docker search imageName
+
+To download image form docker hub  ---->> docker pull imageName
+
+To build the image ------------------->>> docker buiild -it .To
+
+To run the image --------------------->>> docker run -it imageName 
+
+To start the container ----------------->>>docker start conatainerName
+
+To remove conatiner -------------------->>> docker rm conatainerName
+
+To chekc the conatiner we created those are running ------>>> docker ps -a
